44e521585dde3c5c19158d9cfeaa4e796eed9119
marcinzh <marcinzh1@yahoo.com>
2019-03-26 18:03:53 +0100

    add Carrier

---
+++ src/main/scala/fused_effects/Carrier.scala
@@ -0,0 +1,15 @@
+package fused_effects
+import mwords._
+
+
+trait Carrier[H[_[_], _], M[_]] extends HFunctor[H] with Monad[M] {
+  type ThisH = H
+  type ThisM = M
+
+  def eff[A](wtf: H[M, M[A]]): M[A]
+}
+
+
+object Carrier {
+  def apply[H[_[_], _], M[_]] given (ev: Carrier[H, M]) = ev
+}
