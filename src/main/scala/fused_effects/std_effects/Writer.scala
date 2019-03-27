5c9092c6af1df9da6b0111bf2b60e28ea00da1d4
marcinzh <marcinzh1@yahoo.com>
2019-03-27 14:36:23 +0100

    fix wrong Censor signature

--- src/main/scala/fused_effects/std_effects/Writer.scala
+++ src/main/scala/fused_effects/std_effects/Writer.scala
@@ -6,4 +6,4 @@ import mwords._
 sealed trait Writer[W, M[_], A]
 case class Tell[W, M[_], A](stuff: W, wtf: A) extends Writer[W, M, A]
 case class Listen[W, M[_], A, B](scope: M[B], wtf: W => B => A) extends Writer[W, M, A]
-case class Censor[W, M[_], A, B](mod: W => W, scope: M[B], wtf: W => B => A) extends Writer[W, M, A]
+case class Censor[W, M[_], A, B](mod: W => W, scope: M[B], wtf: B => A) extends Writer[W, M, A]
