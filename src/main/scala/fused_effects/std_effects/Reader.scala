637d052170df98095409007d84c984466907fb21
marcinzh <marcinzh1@yahoo.com>
2019-03-29 14:55:21 +0100

    add Reader ops

--- src/main/scala/fused_effects/std_effects/Reader.scala
+++ src/main/scala/fused_effects/std_effects/Reader.scala
@@ -7,8 +7,13 @@ sealed trait Reader[E, M[_], A]
 case class Ask[E, M[_], A](wtf: E => A) extends Reader[E, M, A]
 case class Local[E, M[_], A, B](mod: E => E, scope: M[B], wtf: B => A) extends Reader[E, M, A]
 
+object Reader {
+  type Ap1[E] = [M[_], A] => Reader[E, M, A]
+  type Ap2[E, M[_]] = [A] => Reader[E, M, A]
+}
 
-implied Reader_Effect[E] for Effect[[M[_], A] => Reader[E, M, A]] {
+
+implied Reader_Effect[E] for Effect[Reader.Ap1[E]] {
   private type H = ThisEffect
 
   def (h: H[M, A]) fmap[M[_], A, B](f: A => B): H[M, B] = h match {
@@ -32,3 +37,13 @@ implied Reader_Effect[E] for Effect[[M[_], A] => Reader[E, M, A]] {
       Local(mod, scope2, wtf2)
   }
 }
+
+
+def ask[H[_[_], _], M[_], E] given (evM: Member[Reader.Ap1[E], H], evC: Carrier[H, M]): M[E] =
+  send[Reader.Ap1[E], H, M, E](Ask(evC.theMonad.pure(_)))
+
+def asks[H[_[_], _], M[_], E, A](f: E => A) given (evM: Member[Reader.Ap1[E], H], evC: Carrier[H, M]): M[A] =
+  send[Reader.Ap1[E], H, M, A](Ask(e => evC.theMonad.pure(f(e))))
+
+def local[H[_[_], _], M[_], E, A](f: E => E, scope: M[A]) given (evM: Member[Reader.Ap1[E], H], evC: Carrier[H, M]): M[A] =
+  send[Reader.Ap1[E], H, M, A](Local(f, scope, evC.theMonad.pure(_)))
