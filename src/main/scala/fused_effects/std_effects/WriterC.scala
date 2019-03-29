a7568e74e851b94a6c7851112760501af5975343
marcinzh <marcinzh1@yahoo.com>
2019-03-29 17:01:02 +0100

    WIP: Writer Carrier

--- src/main/scala/fused_effects/std_effects/WriterC.scala
+++ src/main/scala/fused_effects/std_effects/WriterC.scala
@@ -13,14 +13,39 @@ implied WriterC_Carrier[H0[_[_], _] : Effect, M0[_], W: Monoid] given (otherCarr
   private type H = ThisH
   private type M = ThisM
   // import otherCarrier.{theHFunctor => otherHFunctor}
-  // import otherCarrier.{theMonad => otherMonad}
+  import otherCarrier.{theMonad => otherMonad}
   implicit def theHFunctor: HFunctor[H] = the[HFunctor[H]]
   implicit def theMonad: Monad[M] = the[Monad[M]]
 
   def eff[A](h: H[M, M[A]]): M[A] = h match {
-    case Sum.L(Tell(w, wtf)) => ???
-    case Sum.L(Listen(scope, wtf)) => ???
-    case Sum.L(Censor(mod, scope, wtf)) => ???
-    case Sum.R(other) => ???
+    case Sum.L(Tell(w, wtf)) => WriterC(
+      for {
+        _ <- modify((_: W) |@| w)
+        x <- wtf.run
+      } yield x
+    )
+    case Sum.L(Listen(scope, wtf)) => WriterC(
+      for {
+        w0 <- get
+        _ <- put(Monoid[W].empty)
+        a <- scope.run
+        w1 <- get
+        // _ <- modify(w0 |@| (_: W))
+        _ <- put(w0 |@| w1)
+        x <- wtf(w1)(a).run
+      } yield x
+    )
+    case Sum.L(Censor(mod, scope, wtf)) => WriterC(
+      for {
+        w0 <- get
+        _ <- put(Monoid[W].empty)
+        a <- scope.run
+        _ <- modify((w: W) => w0 |@| mod(w))
+        x <- wtf(a).run
+      } yield x
+    )
+    case Sum.R(other) =>
+      // eff (R other) = WriterC (eff (R (handleCoercible other)))
+      ???
   }
 }
